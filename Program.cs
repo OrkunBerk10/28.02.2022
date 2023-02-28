4ed65dc4a8c7a088546bac80fe0b686992a33cc4
OrkunBerk <orkunberk16@gmail.com>
2023-02-28 19:15:59 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿namespace _28._02._2022
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int satir = 5;
+
+            for (int x = 0; x <= satir; x++)
+            {
+                for (int i = satir - x; i >= 0; i--)
+                {
+                    Console.Write("*");
+                }
+
+                for (int k = 1; k <= 2 * x + 1; k++)
+                {
+                    Console.Write(" ");
+                }
+                Console.WriteLine();
+            }
+
+            
+            }
+    }
+}
